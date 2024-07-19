8789f5e73b34816317bbd9f335232bcdfe0bf4c7
Aniyah Curry <aniyahcurry12@gmail.com>
2024-07-19 11:15:25 -0500

    added modal functionality

--- src/pages/gallery_page/components/Gallery/GalleryItem.js
+++ src/pages/gallery_page/components/Gallery/GalleryItem.js
@@ -1,15 +1,28 @@
 import GalleryModal from "./GalleryModal";
 import './GalleryItem.css';
+import { useState } from "react";
 
 function GalleryItem({ item }) {
     const { title, image, description, medium, wip } = item;
+    const [modal, setModal] = useState(false);
+
     return (
-        <div className="item-container">
-            <img src={image}></img>
-            <span className="item_footer">
+        <div>
+            <div className="item-container" onClick={() => setModal(true)}>
+                <img src={image}></img>
+                <span className="item-footer">
+                    <p>{title}</p>
+                </span>
+            </div>
+
+            <GalleryModal
+                openModal={modal}
+                closeModal={() => setModal(false)}
+            >
+                <img className='art' src={image}></img>
                 <h4>{title}</h4>
-                {/* <img src={wip ? star : innovation}></img> */}
-            </span>
+                <p>{description}</p>
+            </GalleryModal>
         </div>
     )
 }
--- src/pages/gallery_page/components/Gallery/GalleryModal.js
+++ src/pages/gallery_page/components/Gallery/GalleryModal.js
@@ -1,5 +1,22 @@
-function GalleryModal() {
-    return <h1>Gallery Modal</h1>
+import { useEffect, useRef } from 'react';
+import './GalleryModal.css';
+import close from '../images/close.png';
+
+function GalleryModal({ children, openModal, closeModal }) {
+    const ref = useRef();
+
+    useEffect(() => {
+        openModal ? ref.current.showModal() : ref.current.close();
+    }, [openModal]);
+
+    return (
+        <div className='gallery-modal'>
+            <dialog ref={ref} onClose={closeModal}>
+                <button onClick={closeModal}>close</button>
+                {children}
+            </dialog>
+        </div>
+    )
 }
 
 export default GalleryModal;
