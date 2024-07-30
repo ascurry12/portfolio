21e301f0d9e2ced36f233086679ba5ffce7848cc
Aniyah Curry <aniyahcurry12@gmail.com>
2024-07-30 09:22:00 -0500

    new stuff

--- src/pages/gallery_page/components/Gallery/FullGallery.js
+++ src/pages/gallery_page/components/Gallery/FullGallery.js
@@ -7,8 +7,34 @@ import testimage4 from '../images/testimage4.png';
 import testimage5 from '../images/testimage5.png';
 import testimage6 from '../images/testimage6.png';
 import testimage7 from '../images/testimage7.png';
+import plumbob from '../images/plumbob.png';
+import star from '../images/star.png';
+import tearoom from '../images/tearoomwip.png';
+
+
+/* type GalleryItemProps = {
+    title: String,
+    image: String,
+    description: String,
+    medium: 'Digital' | 'Physical',
+    wip: boolean
+} */
 
 const galleryItems = [
+    {
+        title: 'Tea Room',
+        image: tearoom,
+        description: 'I made this using blender too!',
+        medium: 'Digital',
+        wip: true
+    },
+    {
+        title: 'Plumbob (by me, actually)',
+        image: plumbob,
+        description: 'I made this using blender!',
+        medium: 'Digital',
+        wip: false
+    },
     {
         title: 'Jake, BMO, and Finn',
         image: testimage,
@@ -21,6 +47,13 @@ const galleryItems = [
         image: testimage2,
         description: 'Jake from \'Adventure Time\'',
         medium: 'Physical',
+        wip: false
+    },
+    {
+        title: 'Blender Star',
+        image: star,
+        description: 'I also made this using blender!',
+        medium: 'Digital',
         wip: false
     },
     {
@@ -57,7 +90,8 @@ const galleryItems = [
         description: 'She\'s the president!',
         medium: 'Digital',
         wip: true
-    }
+    },
+
 
 ]
 
--- src/pages/home_page/Home.js
+++ src/pages/home_page/Home.js
@@ -1,10 +1,38 @@
 import Sparkles from "../gallery_page/components/Sparkles";
+import { Canvas } from '@react-three/fiber/';
+import React, { Suspense } from "react";
+import { useGLTF, OrbitControls } from '@react-three/drei'
+import Scene from './Scene'
+import styled from "styled-components";
+
+useGLTF.preload('/scene.gltf')
+
+function Box() {
+    return (
+
+        <mesh rotation={[0, 10, 0]}>
+            <boxGeometry attach="geometry" args={[5, 5, 5]} />
+            <meshStandardMaterial transparent={true} attach="material" color='purple' opacity={0.5} />
+
+        </mesh>
+
+
+    );
+}
 
 
 function Home() {
     return (
         <div>
             <h1><Sparkles>Summer 2024</Sparkles></h1>
+            <Canvas style={{ height: "50%" }} camera={{ fov: 10, position: [25, 10, 10] }}>
+                <OrbitControls />
+                <Suspense fallback={null}>
+                    <Scene />
+                    <ambientLight intensity={0.1} />
+                    <directionalLight color="white" position={[0, 0, 5]} />
+                </Suspense>
+            </Canvas>
         </div>
     )
 }
